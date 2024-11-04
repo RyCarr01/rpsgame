241e9d3231fcbc67d7dc3545bec3c23a8c88e5cb
RyCarr01 <ryancarroll2177@gmail.com>
2024-11-04 10:03:05 -0500

    finished round

--- js.js
+++ js.js
@@ -16,4 +16,40 @@ function getHumanChoice() {
     humanChoice = prompt("What is your selection: rock, paper, or scissors?");
     return humanChoice;
 }
-console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+
+    if (computerChoice == "rock" && humanChoice == "rock") {
+        console.log("It's a tie! Try again!");
+
+    } else if (computerChoice == "paper" && humanChoice == "paper") {
+        console.log("It's a tie! Try again!");
+
+    } else if (computerChoice == "scissors" && humanChoice == "scissors") {
+        console.log("It's a tie! Try again!");
+    
+    } else if (computerChoice == "rock" && humanChoice == "paper") {
+        console.log("Paper beats rock! User wins this round.");
+    
+    } else if (computerChoice == "rock" && humanChoice == "scissors") {
+        console.log("Rock beats scissors! User loses this round.");
+
+    } else if (computerChoice == "scissors" && humanChoice == "rock") {
+        console.log("Rock beats scissors! User wins this round.");
+
+    } else if (computerChoice == "scissors" && humanChoice == "paper") {
+        console.log("Scissors beat paper! User loses this round.");
+        
+    } else if (computerChoice == "paper" && humanChoice == "rock") {
+        console.log("Paper beats rock! User loses this round.");
+        
+    } else if (computerChoice == "paper" && humanChoice == "scissors") {
+        console.log("Scissors beat paper! User wins this round.");
+        
+    }
+} 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Computer chose " + computerChoice + "...");
+playRound(humanSelection, computerSelection);
